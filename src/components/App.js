878d887c0a67dab5bd41677e81ea8025ed9fba0c
Elizabeth <ek@elizabethkillgore.com>
2023-12-20 17:55:43 -0500

    props basic lab

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,11 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={'firebrick'} name={'Liza'} city={'New York'} />  
+      <About bio={'I made this!'} /> 
     </div>
   );
 }
+ 
 
 export default App;
